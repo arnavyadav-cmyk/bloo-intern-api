018f209463213f397994a3193244a0e2f96add83
Arnav R. Yadav <arnav.yadav@net-mon.net>
2026-07-31 14:22:28 +0530

    Add inventory and user endpoint

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
-app=FastAPI()
+app = FastAPI()
+
+inventory = {
+    1: {
+        "name": "arnav",
+        "role": "intern"
+    },
+    2: {
+        "name": "ashish",
+        "role": "mentor"
+    }
+}
 
 @app.get("/")
 def home():
-    return{"message": "welcome to my app"}
+    return {"message": "welcome to my app"}
 
 @app.get("/hello")
 def hello():
-    return{"message": "hello World"}
+    return {"message": "hello World"}
 
 @app.get("/health")
 def health():
-    return{"status": "healthy"}
+    return {"status": "healthy"}
+
+@app.get("/user/{user_id}")
+def user(
+    user_id: int = Path(
+        ...,
+        description="The ID of the user",
+        ge=1
+    )
+):
+    return inventory[user_id]
